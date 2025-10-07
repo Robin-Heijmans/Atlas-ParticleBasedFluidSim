03329ef09ea333e35044806afa18150113c53546
Robin-Heijmans <robinheijmans@hotmail.com>
2025-10-07 18:37:21 +0200

    wip: trial and error testing physics collisions

--- Plugins/ParticleBasedFluidSimulation/Source/ParticleBasedFluidSimulation/Public/FluidSimulationSystem.h
+++ Plugins/ParticleBasedFluidSimulation/Source/ParticleBasedFluidSimulation/Public/FluidSimulationSystem.h
@@ -76,7 +76,7 @@ private:
     float CollisionDampening = 0.6f;
     float SmoothingRadius = 4.f;
     float ViscosityStrength = 1.f;
-    float Sven = 0.2f;
+    float Sven = 0.1f;
 
     uint32 TableSize = 0; 
     const uint32 HashKey1 = 467;
